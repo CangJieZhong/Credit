2bd6da2989fc21a8de66f9bb95abc1573e372ccd
CangJieZhong <zhonglin1847040@qq.com>
2019-05-29 15:02:48 +0800

    parent project commit

---
+++ credit-parent/credit-core/src/main/java/com/zl/credit/creditcore/CreditCoreApplication.java
@@ -0,0 +1,13 @@
+package com.zl.credit.creditcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCoreApplication.class, args);
+	}
+
+}
