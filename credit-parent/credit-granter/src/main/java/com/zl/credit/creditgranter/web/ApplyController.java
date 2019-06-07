73be3d6afdb2da4277fcd420f3f3a49823078444
long13885772314 <879307944@qq.com>
2019-06-07 15:24:05 +0800

    修改后的审查专员Xml

--- credit-parent/credit-granter/src/main/java/com/zl/credit/creditgranter/web/ApplyController.java
+++ credit-parent/credit-granter/src/main/java/com/zl/credit/creditgranter/web/ApplyController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zl.credit.creditcore.pojo.Apply;
 import com.zl.credit.creditcore.service.ApplyService;
@@ -12,11 +14,12 @@ import com.zl.credit.creditcore.service.ApplyService;
 public class ApplyController {
 	@Autowired
 	private ApplyService applyServiceImpl;
-
+	
+	@ResponseBody
+	@RequestMapping("/ApplyPage.action")
 	public List<Apply> ApplyPage(Integer currentPage, Integer pageSize,
 			String loan_type,String realname,String startTime,String endTime,Integer auditor3_msg) throws Exception{
 		return applyServiceImpl.queryApplyAndUserinfo(currentPage, pageSize, loan_type, realname, startTime, endTime, auditor3_msg);
-
 	}
 
 }
